bf47a58cb7cd080abc8c7843d11393171dd5a6dc
Carlos Aristizábal <39062539+caristi@users.noreply.github.com>
2021-01-13 21:35:59 -0500

    Inicializar proyecto

---
+++ src/main/java/com/tiquete/TiqueteApplication.java
@@ -0,0 +1,13 @@
+package com.tiquete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiqueteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiqueteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiquete/TiqueteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiquete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiqueteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
